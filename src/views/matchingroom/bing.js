5925b32c069125511daf84216b2c6f228a83b378
林品歷 <r11725047@ntu.edu.tw>
2023-04-28 00:31:28 +0800

    init and matchingroom swipe

---
+++ src/views/matchingroom/bing.js
@@ -0,0 +1,7 @@
+export default [
+    'AdelieBreeding_ZH-CN1750945258',
+    'BarcolanaTrieste_ZH-CN5745744257',
+    'RedRocksArches_ZH-CN5664546697',
+    'NationalDay70_ZH-CN1636316274',
+    'LofotenSurfing_ZH-CN5901239545'
+  ]
